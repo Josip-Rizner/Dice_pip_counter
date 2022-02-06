f6947edb06d0bfd60ebfce8d155a58c594bec017
Josip-Rizner <rizner.josip@gmail.com>
2022-02-06 12:11:21 +0100

    Improvements to code

--- dice_pip_counter_from_img.py
+++ dice_pip_counter_from_img.py
@@ -1,13 +1,14 @@
 import cv2
+import numpy as np
 
-path = "pictures/img2.jpg"
+path = "images/img1.jpg"
 
 def showImg(img):
-    img = cv2. resize(img, (600, 500))
+    img = cv2.resize(img, (800, 700))
     cv2.imshow("dice", img)
     cv2.waitKey(0)
 
-def getContours(thresh_img, result_image, blobDetector):
+def getDiceContours(thresh_img, original_img, result_image, pipDetector):
     pipCount = 0
     contours, hierarchy = cv2.findContours(thresh_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
     for contour in contours:
@@ -17,21 +18,21 @@ def getContours(thresh_img, result_image, blobDetector):
         
         if((w)*(h) > thresh_img.shape[0]*thresh_img.shape[1]*0.01):
             cv2.rectangle(result_image, (x, y), (x + w, y + h), (0, 255, 0), 5)
-            pipCount += getPips(thresh_img[y:y+h, x:x+w], y, x+w, blobDetector, result_image)     
+            pipCount += getPips(thresh_img[y:y+h, x:x+w], y, x+w, pipDetector, result_image)     
     return pipCount
   
-def getPips(img, y, x, blobDetector, result_image):
+def getPips(img, y, x, pipDetector, result_image):
     #img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     #img_blur = cv2.GaussianBlur(img_gray, (7,7), 5)
-    # _, thresh = cv2.threshold(img_blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU) 
+    #_, thresh = cv2.threshold(img_blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU) 
     cv2.imshow("pips", img)
     cv2.waitKey(0)
-    keypoints = blobDetector.detect(img)
+    keypoints = pipDetector.detect(img)
     numberOfKeypoitns = len(keypoints)
-    cv2.putText(result_image, str(numberOfKeypoitns), (x, y), cv2.FONT_HERSHEY_SIMPLEX, 5, (0, 0, 255))
+    cv2.putText(result_image, str(numberOfKeypoitns), (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0, 0, 255))
     return numberOfKeypoitns
 
-def getBlobDetector():
+def getPipDetector():
     params = cv2.SimpleBlobDetector_Params()
     params.minThreshold = 150
     params.maxThreshold = 200
@@ -43,26 +44,35 @@ def getBlobDetector():
     params.minConvexity = 0
     return cv2.SimpleBlobDetector_create(params)
 
+def isBlackDominantColorInTheBinaryImage(img):
+    white_pixels = np.count_nonzero(img > 0)
+    black_pixels = np.count_nonzero(img == 0)
+    if(white_pixels > black_pixels):
+        return False
+    return True
+        
            
-original = cv2.imread(path)        
-showImg(original)
-result_image = original.copy()
-img_gray = cv2.cvtColor(original, cv2.COLOR_BGR2GRAY)
+original_img = cv2.imread(path)        
+showImg(original_img)
+result_image = original_img.copy()
+img_gray = cv2.cvtColor(original_img, cv2.COLOR_BGR2GRAY)
 showImg(img_gray)
 img_blur = cv2.GaussianBlur(img_gray, (7,7), 5)
 showImg(img_blur)
-_, tresh = cv2.threshold(img_blur, 210, 245, cv2.THRESH_BINARY)
+_, thresh = cv2.threshold(img_blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
+if(isBlackDominantColorInTheBinaryImage(thresh)==False):
+    thresh = 255 - thresh
+    #_, thresh = cv2.threshold(img_blur, 210, 245, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
 
-blobDetector = getBlobDetector()
-pips = getContours(tresh, result_image, blobDetector)
+pipDetector = getPipDetector()
+pips = getDiceContours(thresh, original_img, result_image, pipDetector)
 
-cv2.putText(result_image, str(pips), (0,result_image.shape[0]-5), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0, 255, 0))
+cv2.putText(result_image, str(pips), (0,result_image.shape[0]-5), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 0, 0))
 
-showImg(tresh)
+showImg(thresh)
 showImg(result_image)
 print(pips)
-
 
 cv2.destroyAllWindows()
 
